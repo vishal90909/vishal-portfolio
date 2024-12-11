7aa73a862fd80a6c32d1ebdde59a41cf71414be2
vishal90909 <testmytest129@gmail.com>
2024-12-12 01:03:53 +0530

    added changes

--- js/script.js
+++ js/script.js
@@ -1,82 +1,159 @@
+// (function($) {
+
+//     "use strict";
+
+//     $(document).ready(function() {
+      
+//       // masonoary //
+
+//       initIsotope();
+
+//       // lightbox
+
+//       lightbox.option({
+//         'resizeDuration': 200,
+//         'wrapAround': true,
+//         'fitImagesInViewport': true
+//       })
+      
+//       /* swiper */
+      
+
+//       var testimonialSwiper = new Swiper(".testimonial-swiper", {
+//         spaceBetween: 20,
+//         pagination: {
+//             el: ".testimonial-swiper-pagination",
+//             clickable: true,
+//           },
+//         breakpoints: {
+//           0: {
+//             slidesPerView: 1,
+//           },
+//           800: {
+//             slidesPerView: 3,
+//           },
+//           1400: {
+//             slidesPerView: 3,
+//           }
+//         },
+//       });
+
+//     }); // End of a document ready
+
+//   // init Isotope
+//   var initIsotope = function() {
+    
+//     $('.grid').each(function(){
+
+//       // $('.grid').imagesLoaded( function() {
+//         // images have loaded
+//         var $buttonGroup = $( '.button-group' );
+//         var $checked = $buttonGroup.find('.is-checked');
+//         var filterValue = $checked.attr('data-filter');
+  
+//         var $grid = $('.grid').isotope({
+//           itemSelector: '.portfolio-item',
+//           // layoutMode: 'fitRows',
+//           filter: filterValue
+//         });
+    
+//         // bind filter button click
+//         $('.button-group').on( 'click', 'a', function(e) {
+//           e.preventDefault();
+//           filterValue = $( this ).attr('data-filter');
+//           $grid.isotope({ filter: filterValue });
+//         });
+    
+//         // change is-checked class on buttons
+//         $('.button-group').each( function( i, buttonGroup ) {
+//           $buttonGroup.on( 'click', 'a', function() {
+//             $buttonGroup.find('.is-checked').removeClass('is-checked');
+//             $( this ).addClass('is-checked');
+//           });
+//         });
+//       // });
+
+//     });
+//   }
+
+
+
+
+// })(jQuery);
+
+
 (function($) {
 
-    "use strict";
+  "use strict";
 
-    $(document).ready(function() {
-      
-      // masonoary //
-
+  $(document).ready(function() {
+    
+      // Initialize Isotope
       initIsotope();
 
-      // lightbox
-
+      // Lightbox
       lightbox.option({
-        'resizeDuration': 200,
-        'wrapAround': true,
-        'fitImagesInViewport': true
-      })
-      
-      /* swiper */
-      
-
-      var testimonialSwiper = new Swiper(".testimonial-swiper", {
-        spaceBetween: 20,
-        pagination: {
-            el: ".testimonial-swiper-pagination",
-            clickable: true,
-          },
-        breakpoints: {
-          0: {
-            slidesPerView: 1,
-          },
-          800: {
-            slidesPerView: 3,
-          },
-          1400: {
-            slidesPerView: 3,
-          }
-        },
+          'resizeDuration': 200,
+          'wrapAround': true,
+          'fitImagesInViewport': true
       });
 
-    }); // End of a document ready
+      // Swiper
+      var testimonialSwiper = new Swiper(".testimonial-swiper", {
+          spaceBetween: 20,
+          pagination: {
+              el: ".testimonial-swiper-pagination",
+              clickable: true,
+          },
+          breakpoints: {
+              0: {
+                  slidesPerView: 1,
+              },
+              800: {
+                  slidesPerView: 3,
+              },
+              1400: {
+                  slidesPerView: 3,
+              }
+          },
+      });
+
+      // Add highlight functionality for the Education section
+      $("#about .h-100").on("click", function() {
+        console.log("Section clicked!"); // Debugging line
+        
+        // Remove the highlight class from other sections
+        $("#about .h-100").removeClass("highlight-border active-section");
+
+        // Add the highlight class to the clicked section
+        $(this).addClass("highlight-border active-section");
+    });
+    
+  }); // End of document ready
 
   // init Isotope
   var initIsotope = function() {
-    
-    $('.grid').each(function(){
-
-      // $('.grid').imagesLoaded( function() {
-        // images have loaded
-        var $buttonGroup = $( '.button-group' );
-        var $checked = $buttonGroup.find('.is-checked');
-        var filterValue = $checked.attr('data-filter');
+      $('.grid').each(function() {
+          var $buttonGroup = $('.button-group');
+          var $checked = $buttonGroup.find('.is-checked');
+          var filterValue = $checked.attr('data-filter');
   
-        var $grid = $('.grid').isotope({
-          itemSelector: '.portfolio-item',
-          // layoutMode: 'fitRows',
-          filter: filterValue
-        });
-    
-        // bind filter button click
-        $('.button-group').on( 'click', 'a', function(e) {
-          e.preventDefault();
-          filterValue = $( this ).attr('data-filter');
-          $grid.isotope({ filter: filterValue });
-        });
-    
-        // change is-checked class on buttons
-        $('.button-group').each( function( i, buttonGroup ) {
-          $buttonGroup.on( 'click', 'a', function() {
-            $buttonGroup.find('.is-checked').removeClass('is-checked');
-            $( this ).addClass('is-checked');
+          var $grid = $('.grid').isotope({
+              itemSelector: '.portfolio-item',
+              filter: filterValue
           });
-        });
-      // });
+  
+          $('.button-group').on('click', 'a', function(e) {
+              e.preventDefault();
+              filterValue = $(this).attr('data-filter');
+              $grid.isotope({ filter: filterValue });
+          });
 
-    });
-  }
-
-
-
+          $buttonGroup.on('click', 'a', function() {
+              $buttonGroup.find('.is-checked').removeClass('is-checked');
+              $(this).addClass('is-checked');
+          });
+      });
+  };
 
 })(jQuery);
